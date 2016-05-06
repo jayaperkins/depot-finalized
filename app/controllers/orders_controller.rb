c6209ecfcab7cacc1ad9624bf56e0d41bf9fe40a
jayaperkins <jayaperkins@gmail.com>
2016-05-05 22:07:49 -0700

    commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,6 +39,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: I18n.t('.thanks') }
         format.json { render action: 'show', status: :created, location: @order }
       else
